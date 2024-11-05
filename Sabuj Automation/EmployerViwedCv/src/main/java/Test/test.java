42faeb6b282a6296c82111e68995aba92688f7ac
Sabuj Kumar Modak <120724779+xamiron@users.noreply.github.com>
2024-11-05 18:04:53 +0600

    up

--- Sabuj Automation/EmployerViwedCv/src/main/java/Test/test.java
+++ Sabuj Automation/EmployerViwedCv/src/main/java/Test/test.java
@@ -136,7 +136,7 @@ public class test {
             FromDateField.clear();
             ToDateField.clear();
 
-            // Invalid Date
+            // Invalid Date Search
             FromDateField.sendKeys("2024");
             Thread.sleep(2000);
             ToDateField.sendKeys("05");
